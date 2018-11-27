7ee85145ef13add0d44092ec0457eafa104d8508
Francesco S. Comisso <mitramdhir@gmail.com>
2018-11-27 11:02:28 +0100

    wip

---
+++ main.py
@@ -0,0 +1,24 @@
+
+print("Hello  Bearzi\n")
+
+
+nome= "Mario"
+print("""
+sd
+ a  
+  sd a
+  sd    asd
+    asda
+    ds""")
+
+print(nome.index("r"))
+
+print(nome.replace("io", "te"))
+
+print(nome.upper())
+
+v = 3;
+
+t = 6 * v;
+
+
