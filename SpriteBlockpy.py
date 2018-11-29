1d3b10adc351aab5fb778a037f3482a199df4790
Francesco S. Comisso <mitramdhir@gmail.com>
2018-11-29 01:38:43 +0100

    wip

--- SpriteBlockpy.py
+++ SpriteBlockpy.py
@@ -2,31 +2,46 @@ class Ofset:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.dx = x
+        self.dy = y
+
+    def setDefault(self):
+        self.x = self.dx
+        self.y = self.dy
 
 
-class SpriteBlcok:
+class SpriteBlock:
 
-    def __init__(self, pygame):
-        self.x_size = 16
-        self.y_size = 16
-        self.x_base = 16
-        self.y_base = 16
-        self.x_coord = 16
-        self.y_coord = 16
+    def __init__(self, pygame, imgpath):
+        self.x_size = 0
+        self.y_size = 0
+        self.x_base = 0
+        self.y_base = 0
+        self.x_coord = 0
+        self.y_coord = 0
+        self.ofset = None
+        self.base_ofset = None
 
-        self.image = pygame.image.load("Sprites/Old hero.png").convert()
+        self.image = pygame.image.load(imgpath).convert()
 
         color = self.image.get_at((0, 0))
 
         self.image.set_colorkey(color)
 
-    def draw(self, screen, ofset):
-        clip_rect = (self.x_base + ofset.x, self.y_base + ofset.y, self.x_size, self.y_size)
+    def draw(self, screen):
+        print(self.ofset.x)
+        clip_rect = (self.x_base + self.ofset.x, self.y_base + self.ofset.y, self.x_size, self.y_size)
 
         screen.blit(self.image, [self.x_coord, self.y_coord], clip_rect)
 
+    def setPosition(self, x, y):
+        self.x_coord, self.y_coord = x, y
 
-class Puppet(SpriteBlcok):
+    def getSize(self):
+        return self.x_size, self.y_size
+
+
+class Puppet(SpriteBlock):
     PUPPET_STAND = 0
     PUPPET_RUN = 1
     PUPPET_SWIM = 2
@@ -34,23 +49,96 @@ class Puppet(SpriteBlcok):
 
     def __init__(self, pygame):
 
-        super().__init__(self, pygame)
+        super().__init__(pygame, "Sprites/Old hero.png")
 
-        self.ofset =  Ofset(16,17)
+        self.x_size = 16
+        self.y_size = 16
+        self.x_base = 16
+        self.y_base = 15
+        self.x_coord = 16
+        self.y_coord = 16
+
+        self.base_ofset = Ofset(16, 17)  # into the sprite system coordinate
+        self.ofset = Ofset(16, 17)  # into the sprite system coordinate
         self.time = 0
-
-    def draw(self, screen):
-
-        super().draw(self, screen, self.ofset)
+        self.animation_step = 0
+        self.state = Puppet.PUPPET_STAND
 
     def tick(self):
         self.time += 1
 
-        if self.state == Puppet.PUPPET_STAND:
-            pass
+        if self.state == Puppet.PUPPET_RUN:
 
-        elif self.state == Puppet.PUPPET_RUN:
-            pass
+            self.ofset.y = self.base_ofset.y * 1
+            if self.time % 15 == 0:
+                self.animation_step += 1
+                self.animation_step = self.animation_step % 6
+                self.ofset.x = self.base_ofset.x * self.animation_step
+
+        elif self.state == Puppet.PUPPET_STAND:
+
+            self.ofset.y = self.base_ofset.y * 0
+            if self.time % 15 == 0:
+                self.animation_step += 1
+                self.animation_step = self.animation_step % 3
+                self.ofset.x = self.base_ofset.x * self.animation_step
 
     def setState(self, state):
         self.state = state
+        self.ofset.setDefault()
+
+
+class Tile(SpriteBlock):
+    SX= SY =15
+
+    def __init__(self, pygame):
+        super().__init__(pygame, "Sprites/psygen.png")
+
+        self.base_ofset = Ofset(0, 0)  # into the sprite system coordinate
+        self.ofset = Ofset(0, 0)  # into the sprite system coordinate
+        self.y_size, self.x_size = Tile.SX, Tile.SY
+
+
+class Wall():
+
+    def __init__(self, pygame, base_x, base_y):
+
+        self.tiles = []
+
+        for i in range(0, 10):
+            self.tiles.append(Tile(pygame))
+
+            self.tiles[i].setPosition(base_x, base_y + i * Tile.SY)
+
+    def draw(self, screen):
+        for t in self.tiles:
+            t.draw(screen)
+
+    def setPosition(self, x, y):
+
+        #print(self.tiles)
+        for i in range(0, len(self.tiles)):
+            (a, b) = (self.tiles[i].getSize())
+            self.tiles[i].setPosition(x, y + i * b)
+
+class Floar():
+
+    def __init__(self, pygame, base_x, base_y):
+
+        self.tiles = []
+
+        for i in range(0, 10):
+            self.tiles.append(Tile(pygame))
+
+            self.tiles[i].setPosition(base_x + i * Tile.SX, base_y )
+
+    def draw(self, screen):
+        for t in self.tiles:
+            t.draw(screen)
+
+    def setPosition(self, x, y):
+
+        #print(self.tiles)
+        for i in range(0, len(self.tiles)):
+            (a, b) = (self.tiles[i].getSize())
+            self.tiles[i].setPosition(x + i * b, y )
---
+++ game1.py
@@ -0,0 +1,120 @@
+
+
+# Import a library of functions called 'pygame'
+import pygame
+from SpriteBlockpy import *
+
+
+# Initialize the game engine
+pygame.init()
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+
+WHITE = (0xFF, 0xFF, 0xFF)
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+
+x_speed = 0
+y_speed = 0
+
+# Current position
+x_coord = 10
+y_coord = 10
+y_coord = 10
+
+
+t, t1 = 0 ,0
+time=0
+
+pupp = Puppet(pygame)
+
+tile = Tile(pygame)
+
+wall = Wall(pygame,0,0)
+
+floar1 = Floar(pygame,200,100)
+floar2 = Floar(pygame,400,0)
+
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        # User pressed down on a key
+        elif event.type == pygame.KEYDOWN:
+            # Figure out if it was an arrow key. If so
+            # adjust speed.
+            if event.key == pygame.K_LEFT:
+                x_speed = -3
+                pupp.setState(Puppet.PUPPET_RUN)
+            elif event.key == pygame.K_RIGHT:
+                x_speed = 3
+                pupp.setState(Puppet.PUPPET_RUN)
+            elif event.key == pygame.K_UP:
+                y_speed = -3
+                pupp.setState(Puppet.PUPPET_RUN)
+            elif event.key == pygame.K_DOWN:
+                y_speed = 3
+                pupp.setState(Puppet.PUPPET_RUN)
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                x_speed = 0
+                pupp.setState(Puppet.PUPPET_STAND)
+            elif event.key == pygame.K_RIGHT:
+                x_speed = 0
+                pupp.setState(Puppet.PUPPET_STAND)
+            elif event.key == pygame.K_UP:
+                y_speed = 0
+                pupp.setState(Puppet.PUPPET_STAND)
+
+            elif event.key == pygame.K_DOWN:
+                y_speed = 0
+                pupp.setState(Puppet.PUPPET_STAND)
+
+
+    # --- Game logic should go here
+
+    # --- Drawing code should go here
+    # First, clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    screen.fill(WHITE)
+
+    x_coord+=x_speed
+    y_coord += y_speed
+
+    #screen.blit(background, [0,0])
+
+    pupp.setPosition(x_coord,y_coord)
+    pupp.tick()
+    pupp.draw(screen)
+
+    tile.setPosition(200,100)
+    tile.draw(screen)
+
+    wall.setPosition(300,100)
+    wall.draw(screen)
+    floar1.draw(screen)
+    floar2.draw(screen)
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    time+=1
+    clock.tick(60)
